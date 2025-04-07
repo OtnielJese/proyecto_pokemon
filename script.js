c1ddc638d5ff09fab81a8598a36a71ae5318302e
OtnielJese <otnieljes3@gmail.com>
2025-04-07 11:22:15 -0500

    feat: se agrego funcion al boton, y se creo archivo JS

---
+++ script.js
@@ -0,0 +1,11 @@
+const url = " https://pokeapi.co/api/v2/pokemon/";
+const card = document.getElementById("card");
+const btn = document.getElementById("btn");
+
+let getPokeData = () => {
+  let id = Math.floor(Math.random() * 150) + 1;
+console.log(id)
+}
+btn.addEventListener("click", getPokeData);
+
+
